23ba3613f7975ed3966372f04d6b0b89594259fe
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-12-29 21:40:02 +0330

    config => tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+
+      boxShadow: {
+        normal: "0px 1px 10px 0px #0000000D",
+      },
+
+      borderRadius: {
+        "4xl": "2rem",
+      },
+
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemiBold: "Dana DemiBold",
+        MorabbaLight: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+    },
   },
   plugins: [],
 };
